96b6d89c20411a53c3bdfd6f1601ddfc1079148c
Kiril Chuchev <54711535+KirilChuchev@users.noreply.github.com>
2021-04-04 19:06:10 +0300

    implement searchSets list && searchSet card stylization

--- BgPropertiesClient/bg-properties-client/src/components/SearchSets/SearchSetShortDetails/SearchSetShortDetails.js
+++ BgPropertiesClient/bg-properties-client/src/components/SearchSets/SearchSetShortDetails/SearchSetShortDetails.js
@@ -1,16 +1,21 @@
-import { Fragment } from "react";
+import styles from './SearchSetShortDetails.module.css'
 
-const SearchSetShortDetails = ({ searchSet }) => {
+const SearchSetShortDetails = ({ searchSet, index }) => {
   function parseTime(str) {
-    let date = new Date();
-    return date.toString(str);
+    let date = Date(str);
+    return date;
   }
   return (
-    <Fragment>
-      <p>{searchSet.name}</p>
-      <p>{searchSet.bgPropertiesCount}</p>
-      <p>{parseTime(searchSet.createdOn)}</p>
-    </ Fragment>
+    <article className={styles.searchSetShortDetailsArticle}>
+      <section className={styles.nameSection}>
+        <p>№: {index}</p>
+        <p>Име: {searchSet.name}</p>
+      </section>
+      {/* <p>Създаден на: {parseTime(searchSet.createdOn)}</p> */}
+      <p className={styles.createdOn}>Създаден на: {parseTime(searchSet.createdOn)}</p>
+      <p className={styles.bgPropertiesCount}>Брой обяви: {searchSet.bgPropertiesCount}</p>
+      <p className={styles.description}>Описание: {searchSet.description ? searchSet.description.slice(0, 30).trim() + "..." : "n/a"}</p>
+    </article>
   );
 };
 
